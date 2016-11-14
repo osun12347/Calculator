ff71783c56bf24daddd8639fbb1cda508b3f521f
Neal <Neal@V-DOXIE-W10>
2016-11-14 18:41:20 +0800

    Add project files.

---
+++ Calculator/MathCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Calculator
+{
+    public class MathCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var model= parameter as ViewModel;
+            if (model!=null)
+            {
+                model.Result = model.Param1 / model.Param2;
+                int a = 0;
+                int result = 10 / a;
+            }
+           
+        }
+    }
+}
---
+++ Calculator/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class ViewModel:INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+            ButtonClick = new MathCommand();
+        }
+        public double Param1 { get; set; }
+        public double Param2 { get; set; }
+        //public double Result { get; set; }
+        private double _Result;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double Result
+        {
+            get { return _Result; }
+            set { _Result = value;
+                if (PropertyChanged!=null)
+                {
+                    PropertyChanged(this,new PropertyChangedEventArgs("Result"));
+                }
+            }
+        }
+
+        public MathCommand ButtonClick { get; set; }
+    }
+}
